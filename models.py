b1fabd05d0ecab61459f638d99b0b0e583125cb9
Ballauf Maximilian <q410626@europe.bmw.corp>
2021-05-28 20:23:04 +0200

    databaselink

--- models.py
+++ models.py
@@ -3,10 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 import json
 import os
 
-database_name = "capstone"
-database_path = "postgres://{}/{}".format('postgres:0000@localhost:5432', database_name)
+#database_name = "capstone"
+#database_path = "postgres://{}/{}".format('postgres:0000@localhost:5432', database_name)
 
-#database_path = os.environ['DATABASE_URL']
+database_path = os.environ['DATABASE_URL']
 
 db = SQLAlchemy()
 
